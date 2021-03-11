ee8c5a682155246f9d20c8e7a920b6989153a787
fachrisyam <61930556+fachrisyam@users.noreply.github.com>
2021-03-11 08:52:58 +0800

    Add files via upload

---
+++ Tugas 1 Muh. Fachrisyam A3/Incr.java
@@ -0,0 +1,13 @@
+/* Efek dari operator ++ */
+public class Incr {
+
+	public static void main(String[] args) {
+
+		int i, j;
+
+		i = 3;
+		j = i++;
+		System.out.println ("Nilai i : " + (++i) +
+		"\nNilai j : " + j);
+	}
+}
